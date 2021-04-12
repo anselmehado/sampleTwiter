01100a31e2b479dc28eb47f64682d968a2ec3c85
anselmehado <anselmehado85@yahoo.com>
2021-04-12 06:35:49 +0100

    la fonction delete ne marche pas

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,65 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
 
   def show
+    #@post = Post.find(params[:id])
   end
 
-  def update
-  end
 
-  def destroy
-  end
 
   def edit
+     #@post = Post.find(params[:id])
   end
 
   def new
     @post = Post.new
 
   end
+
+  def create
+    @post = Post.new(post_params)
+      if @post.save
+        # 一覧画面へ遷移して"I have created a blog!"とメッセージを表示します。
+     redirect_to posts_path, notice: "I have created a post"
+   else
+     render :new
+   end
+    # renvoi une nvlle page pour poster a nouveau un blog redirect_to
+    #"/blogs/new"= redirect_to new_blog_path
+    #redirect_to "/blogs/new"
+    #redirect_to new_blog_path
+  end
+
+  def update
+
+    #@blog = Blog.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "I edited the blog!"
+    else
+      render :edit
+    end
+  end
+
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice:"I deleted the blog!"
+  end
+
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:Autheur, :content)
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  # get 'posts/index'
-  # get 'posts/show'
-  # get 'posts/update'
-  # get 'posts/destroy'
-  # get 'posts/edit'
-  # get 'posts/new'
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/update'
+  get 'posts/destroy'
+  get 'posts/edit'
+  get 'posts/new'
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
